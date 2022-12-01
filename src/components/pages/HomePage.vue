e7d0680835c152d846d660b4608ba1c870a4ffd7
Eduardo Bessa <eduubessa@gmail.com>
2022-12-01 20:37:43 +0000

    HomePage complete

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -6,16 +6,34 @@
   <section class="page" id="home">
     <div class="container-fluid">
       <div class="row">
-        <header clasS="header-page" id="header-page-home">
+        <div class="col col-left flex-center">
+          <hr />
+          <div class="social">
+            <a href="#">
+              <i class="ri-whatsapp-line"></i>
+            </a>
+            <a href="#">
+              <i class="ri-telegram-line"></i>
+            </a>
+            <a href="#">
+              <i class="ri-group-line"></i>
+            </a>
+          </div>
+          <hr />
+        </div>
+        <div class="col col-middle flex-center">
           <div class="container">
             <h1 class="text-white">Bem-vindo ao espaço</h1>
             <h1 class="text-white font-bold">Creative</h1>
-            <h3 class="text-white">
+            <h2 class="text-white">
               Um web site cheio de informaçõe sobre tecnologia e uma comunidade onde voce pode expandir seu network e
               compartilhar seus trabalhos.
-            </h3>
+            </h2>
           </div>
-        </header>
+        </div>
+        <div class="col col-right flex-center">
+          <img src="/images/Community-icon.svg" alt="Creative Community">
+        </div>
       </div>
     </div>
   </section>
